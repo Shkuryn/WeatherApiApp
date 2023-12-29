2c6161c0c1c21dc35791753c75587d97f405cdf8
ihar.shkuryn <ihar.shkuryn@softswiss.com>
2023-12-29 15:12:08 +0300

    added model

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,13 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2023_12_29_121039) do
+  create_table "forecasts", force: :cascade do |t|
+    t.decimal "temperature"
+    t.datetime "epoch_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
