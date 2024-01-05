1f54c1569982bfb68daa6b3b4486b6138f7d1128
ihar.shkuryn <ihar.shkuryn@softswiss.com>
2024-01-05 21:59:26 +0300

    rubocop

--- spec/services/accuweather_service_spec.rb
+++ spec/services/accuweather_service_spec.rb
@@ -7,24 +7,24 @@ RSpec.describe AccuweatherService do
     context 'when empty data' do
       it 'fetches and saves weather data' do
         VCR.use_cassette('accuweather_service') do
-          expect {
+          expect do
             AccuweatherService.call
-          }.to change { Forecast.count }.by(24)
+          end.to change { Forecast.count }.by(24)
         end
       end
     end
     context 'when data exist', :aggregate_failures do
-      let(:epoch_time_1) { (Time.current.beginning_of_hour - 1.hour).to_i }
-      let(:epoch_time_2) { (Time.current.beginning_of_hour - 2.hour).to_i }
-      let!(:forecast1) { FactoryBot.create(:forecast, epoch_time: epoch_time_1, temperature: 10) }
-      let!(:forecast2) { FactoryBot.create(:forecast, epoch_time: epoch_time_2, temperature: 15) }
+      let(:epochtime1) { (Time.current.beginning_of_hour - 1.hour).to_i }
+      let(:epochtime2) { (Time.current.beginning_of_hour - 2.hour).to_i }
+      let!(:forecast1) { FactoryBot.create(:forecast, epoch_time: epochtime1, temperature: 10) }
+      let!(:forecast2) { FactoryBot.create(:forecast, epoch_time: epochtime2, temperature: 15) }
 
       it 'update temperature' do
-        expect {
+        expect do
           VCR.use_cassette('accuweather_service') do
             AccuweatherService.call
           end
-        }.to change { Forecast.count }.from(2).to(24)
+        end.to change { Forecast.count }.from(2).to(24)
       end
     end
   end
